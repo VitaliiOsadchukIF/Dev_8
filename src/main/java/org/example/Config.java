8bacf9b942ad4f0988c771d9af5b869c04ac0676
VitaliiOsadchukIF <radistje@gmail.com>
2023-09-16 15:12:08 +0300

    update2 Dev_8

--- src/main/java/org/example/Config.java
+++ src/main/java/org/example/Config.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Config {
 
-    private Config() {}
+    private Config() {
+    }
 
     public static String jdbcUrl = "jdbc:mysql://localhost:3306/osbb10";
     public static String userNane = "root";
